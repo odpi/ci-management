1e7f5b5701cf7a9e14c694db5fbdb709b6d31ac0
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-10-09 20:39:30 -0500

    Add Jenkins global config vars
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ jenkins-config/global-vars-sandbox.sh
@@ -0,0 +1,7 @@
+GIT_BASE=https://github.com/odpi/$PROJECT
+GIT_CLONE_URL=git@github.com:
+GIT_URL=https://github.com
+JENKINS_HOSTNAME=vex-yul-odpi-jenkins-sandbox-1
+LOGS_SERVER=https://logs.odpi.org
+NEXUS_URL=https://nexus.odpi.org
+SILO=sandbox
