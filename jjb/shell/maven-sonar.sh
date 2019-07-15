178a6b783e200db5f60ab592793e0b9d72830e9f
Suresh Channamallu <schannamallu@linuxfoundation.org>
2019-07-15 14:49:17 -0400

    Add mvn spotbugs and pmd scan in sonar job
    Signed-off-by: Suresh Channamallu <schannamallu@linuxfoundation.org>

---
+++ jjb/shell/maven-sonar.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> maven-sonar.sh"
+# This script builds a Maven project and deploys it into a staging repo which
+# can be used to deploy elsewhere later eg. Nexus staging / snapshot repos.
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -xe -o pipefail
+set +u
+
+export MAVEN_OPTS
+
+declare -a params
+params+=("--global-settings $GLOBAL_SETTINGS_FILE")
+params+=("--settings $SETTINGS_FILE")
+
+# Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+# shellcheck disable=SC2086
+_JAVA_OPTIONS="-Xmx5G" $MVN clean install \
+    -e -Dsonar \
+    ${params[*]} \
+    $MAVEN_OPTIONS $MAVEN_PARAMS
+
+if [ "$SONAR_HOST_URL" = "https://sonarcloud.io" ]; then
+    params+=("-Dsonar.projectKey=$PROJECT_KEY")
+    params+=("-Dsonar.organization=$PROJECT_ORGANIZATION")
+    params+=("-Dsonar.login=$API_TOKEN")
+fi
+
+# Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+# shellcheck disable=SC2086
+"$MVN" $SONAR_MAVEN_GOAL \
+    -e -Dsonar -Dsonar.host.url="$SONAR_HOST_URL" \
+    ${params[*]} \
+    $MAVEN_OPTIONS $MAVEN_PARAMS
